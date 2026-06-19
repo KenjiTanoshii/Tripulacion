ead45bc8bf884be681e838da27aa5be44f8a72c6
KenjiTanoshii <destello_morado_daishu@hotmail.com>
2026-06-19 23:54:12 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,336 @@
+const pages = [
+
+{
+image:"https://i.gyazo.com/a3b4b0da6d7b7e43235ff2888a9476f0.png",
+
+content:`
+<div class="page-layout">
+
+    <div class="history">
+
+        <h1>Historia</h1><div style="background: url(https://i.imgur.com/tRziMH2.png); background-size: 100%; align-items: center; width: 230px; height: 230px; margin-top: -30px;"></div>
+        <p>
+            Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas "Letraset", las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.
+        </p>
+
+        <p>
+            Cómo nació, quién la fundó,
+            sus objetivos y su legado.
+        </p>
+
+    </div>
+
+</div>
+`
+},
+
+{
+image:"https://i.gyazo.com/34afa795b99821c6c9a6566fddc8f9e1.png",
+
+content:`
+<div class="page-layout">
+
+    <div class="left-page">
+
+        <h1>Integrantes</h1>
+
+        <table border="0" cellspacing="0" style="margin-left:0px; border-collapse: separate; border-spacing: 0 15px; margin-top: -25px;">
+        <tbody><tr>
+        <td><div style="width: 100px; height: 123px; margin-right: 10px; background: url(https://i.imgur.com/8fdZRYC.png); background-size: 100%; z-index: 2"><div style="transform: translate(16.5px, 20px); width: 68px; height: 86px; position: relative; border-radius: 100%; background: url(https://i.gyazo.com/b89a48dac67c6c39185d5674b979f27c.png) center; background-size: cover; z-index: 1"></div></div></td><td><b>Haedir, Rey del Inframundo</b><br>
+        <u>Cargo:</u> Capitán
+        <u>WANTED:</u> ฿ </td></tr>
+        
+        <tr>
+        <td><div style="width: 100px; height: 123px; margin-right: 10px; background: url(https://i.imgur.com/8fdZRYC.png); background-size: 100%; z-index: 2"><div style="transform: translate(16.5px, 20px); width: 68px; height: 86px; position: relative; border-radius: 100%; background: url(https://i.imgur.com/a8gSRat.jpeg) center; background-size: cover; z-index: 1"></div></div></td><td><b>Red, Reina de Corazones</b><br>
+        <u>Cargo:</u> Subcapitana
+        <u>WANTED:</u> ฿ </td></tr>
+        
+        <tr>
+        <td><div style="width: 100px; height: 123px; margin-right: 10px; background: url(https://i.imgur.com/8fdZRYC.png); background-size: 100%; z-index: 2"><div style="transform: translate(16.5px, 20px); width: 68px; height: 86px; position: relative; border-radius: 100%; background: url(https://i.imgur.com/chTaMSi.png) center; background-size: cover; z-index: 1"></div></div></td><td><b>Morgana Strega, Buona Madre</b><br>
+        <u>Cargo:</u> Cocinera
+        <u>WANTED:</u> ฿ </td></tr>
+
+        <tr>
+        <td><div style="width: 100px; height: 123px; margin-right: 10px; background: url(https://i.imgur.com/8fdZRYC.png); background-size: 100%; z-index: 2"><div style="transform: translate(16.5px, 20px); width: 68px; height: 86px; position: relative; border-radius: 100%; background: url(https://i.gyazo.com/6458b8c57d4997336139263ff6a48fdf.png) center; background-size: cover; z-index: 1"></div></div></td><td><b>Evie</b><br>
+        <u>Cargo:</u> Navegante
+        <u>WANTED:</u> ฿ </td></tr>
+        
+        <tr>
+        <td><div style="width: 100px; height: 123px; margin-right: 10px; background: url(https://i.imgur.com/8fdZRYC.png); background-size: 100%; z-index: 2"><div style="transform: translate(16.5px, 20px); width: 68px; height: 86px; position: relative; border-radius: 100%; background: url(https://i.imgur.com/rdhoeh0.jpeg) center; background-size: cover; z-index: 1"></div></div></td><td><b>Ishtar</b><br>
+        <u>Cargo:</u> Navegante
+        <u>WANTED:</u> ฿ </td></tr></tbody></table>
+
+    </div>
+
+    <div class="right-page">
+
+        <h1>Barco</h1>
+        <b>Nombre de barco</b>
+<ul><li>Proa:</li>
+<li>Popa:</li>
+<li>Lateral:</li>
+<li>Casco:</li>
+<li>Otros:</li>
+</ul>
+
+Mejoras Generales:<br>
+- <br><br>
+
+Complementos para Barco:<br>
+-
+
+    </div>
+
+</div>
+`
+},
+
+{
+image:"https://i.gyazo.com/34afa795b99821c6c9a6566fddc8f9e1.png",
+
+content:`
+<div class="page-layout">
+
+    <div class="left-page">
+
+        <h1>Tripulación</h1>
+<b>Tipo de vida:</b> Morganeer <br>
+<b>Nivel de tripulación:</b> V <br>
+<b>Jolly Points:</b> <span style="color: green">00</span> / <span style="color: red">00</span> / <span style="color: orange">00</span> <br>
+<b>Mejoras de la tripulación:</b> <br>
+- <br>
+- <br><br>
+
+<b>Logros de la tripulación:</b> <br>
+- <br>
+- <br><br>
+
+<b>Monedas totales:</b> <span style="color: bronze">0</span> / <span style="color: silver">0</span> / <span style="color: gold">0</span>
+
+    </div>
+
+    <div class="right-page">
+
+        <h1>Registro</h1>
+<b>19/06/2026</b> - La tripulación es fundada por <b>Haedir</b>.<br>
+
+    </div>
+
+</div>
+`
+}
+
+];
+
+let current = 0;
+
+const page =
+document.getElementById("page");
+
+const pageContent =
+document.getElementById("pageContent");
+
+const pageFlip =
+document.getElementById("pageFlip");
+
+const cover =
+document.getElementById("cover");
+
+const book =
+document.getElementById("book");
+
+const nextBtn =
+document.getElementById("nextBtn");
+
+const prevBtn =
+document.getElementById("prevBtn");
+
+/* ==========================
+   RENDER
+========================== */
+
+function renderPage(){
+
+page.style.backgroundImage =
+`url("${pages[current].image}")`;
+
+pageContent.innerHTML =
+pages[current].content;
+
+}
+
+/* ==========================
+   ABRIR LIBRO
+========================== */
+
+cover.addEventListener("click",()=>{
+
+if(cover.classList.contains("open")) return;
+
+cover.classList.add("open");
+
+setTimeout(()=>{
+
+book.classList.add("visible");
+
+},700);
+
+});
+
+/* ==========================
+   VOLVER A PORTADA
+========================== */
+
+function goToCover(){
+
+book.classList.remove("visible");
+
+cover.classList.remove("open");
+
+/* ocultamos contenido */
+
+pageContent.style.opacity = "0";
+
+setTimeout(()=>{
+
+current = 0;
+
+renderPage();
+
+pageContent.style.opacity = "1";
+
+},1600);
+
+}
+
+/* ==========================
+   ANIMACIÓN DE PÁGINA
+========================== */
+
+function animatePageTurn(nextIndex){
+
+if(nextIndex < 0) return;
+
+if(nextIndex >= pages.length) return;
+
+pageFlip.classList.add("flipping");
+
+setTimeout(()=>{
+
+current = nextIndex;
+
+renderPage();
+
+},700);
+
+setTimeout(()=>{
+
+pageFlip.classList.remove("flipping");
+
+},1400);
+
+}
+
+/* ==========================
+   CIERRE CON ANIMACIÓN
+========================== */
+
+function animateCloseBook(){
+
+pageFlip.classList.add("flipping");
+
+setTimeout(()=>{
+
+goToCover();
+
+},700);
+
+setTimeout(()=>{
+
+pageFlip.classList.remove("flipping");
+
+},1400);
+
+}
+
+/* ==========================
+   SIGUIENTE
+========================== */
+
+nextBtn.addEventListener("click",()=>{
+
+if(current < pages.length - 1){
+
+animatePageTurn(current + 1);
+
+}
+else{
+
+animateCloseBook();
+
+}
+
+});
+
+/* ==========================
+   ANTERIOR
+========================== */
+
+prevBtn.addEventListener("click",()=>{
+
+if(current > 0){
+
+animatePageTurn(current - 1);
+
+}
+else{
+
+goToCover();
+
+}
+
+});
+
+/* ==========================
+   TECLADO
+========================== */
+
+document.addEventListener("keydown",(e)=>{
+
+if(e.key === "ArrowLeft"){
+
+if(current > 0){
+
+animatePageTurn(current - 1);
+
+}
+else{
+
+goToCover();
+
+}
+
+}
+
+if(e.key === "ArrowLeft"){
+
+if(current > 0){
+
+animatePageTurn(current - 1);
+
+}
+else{
+
+animateCloseBook();
+
+}
+
+}
+
+});
+
+/* ==========================
+   INICIO
+========================== */
+
+renderPage();
